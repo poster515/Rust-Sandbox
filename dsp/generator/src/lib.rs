9da350bd6b2f79079f365fdd0010427f31042237
Joseph Post <jpost625@gmail.com>
2023-12-28 17:37:41 -0500

    Fixing wave generation functions
    Need to test but otherwise the ported versions compile

--- dsp/generator/src/lib.rs
+++ dsp/generator/src/lib.rs
@@ -1,19 +1,22 @@
 
 
 use num_traits::Num;
+use num::cast::AsPrimitive;
 use num_traits::identities::one;
+
+use std::f64::consts::PI;
 
 use std::sync::{Arc, Mutex};
 
-pub fn generate_square_wave_data<T: Num + std::ops::Neg<Output = T> + std::fmt::Display>(vec: &mut Vec<T>
+pub fn generate_square_wave_data<T: Num + std::ops::Neg<Output = T> + std::fmt::Display + Copy>(vec: &mut Vec<T>
 		, buffer_len: usize
 		, clock: Arc<Mutex<i8>>
 		, period: i64){
-	let mut val: T;
 
+	// get the current clock value
+	let mut val: T;
 	let value_guard = clock.lock().unwrap();
-	let current_clock_value: i8 = *value_guard;
-	let mut temp_clock_value: i8 = current_clock_value;
+	let mut temp_clock_value: i8 = *value_guard;
 	drop(value_guard);
 
 	for i in 0..buffer_len {
@@ -34,46 +37,85 @@ pub fn generate_square_wave_data<T: Num + std::ops::Neg<Output = T> + std::fmt::
 	}
 }
 
-// template <typename T>
-// pub fn generateSineWaveData(T ** buffer, const int channel){
+pub fn generate_sine_wave_data(vec: &mut Vec<f64>
+		, buffer_len: usize
+		, clock: Arc<Mutex<i8>>
+		, period: i64){
 
-// 	for (int i = 0; i < BUFFER_LEN; i++){
-// 		bool updated = false;
-// 		while(CLOCK == 0){}
-// 		while(CLOCK == 1){
-// 			if (!updated){
-// 				updated = true;
-// 				buffer[channel][i] = (T)sin((2 * PI * (i % N)) / N);
-// 			}
-// 		}
-// 	}
-// }
+	// get the current clock value
+	let mut val: f64;
+	let value_guard = clock.lock().unwrap();
+	let mut temp_clock_value: i8 = *value_guard;
+	drop(value_guard);
 
-// template <typename T>
-// pub fn generateTriangleWaveData(T ** buffer, const int channel){
-// 	for (int i = 0; i < BUFFER_LEN; i++){
-// 		bool updated = false;
-// 		while(CLOCK == 0){}
-// 		while(CLOCK == 1){
-// 			if (!updated){
-// 				updated = true;
-// 				buffer[channel][i] = (i % N) < (N/2) ? (T)(4*(i % N))/N - 1 : 3 - (T)(4*(i % N))/N;
-// 			}
-// 		}
-// 	}
-// }
+	for i in 0..buffer_len {
+		while temp_clock_value == 0 {
+			temp_clock_value = *clock.lock().unwrap();
+		}
+		val = ((2.0 * PI * (i as f64 % period as f64)) / period as f64).sin();
+		// val = ((2.0 * PI * (i as f64 % period as f64)) / period as f64).sin().as_();
+		(*vec).push(val);
 
-// template <typename T>
-// pub fn generateSawtoothWaveData(T ** buffer, const int channel){
-// 	for (int i = 0; i < BUFFER_LEN; i++){
-// 		bool updated = false;
-// 		while(CLOCK == 0){}
-// 		while(CLOCK == 1){
-// 			if (!updated){
-// 				updated = true;
-// 				buffer[channel][i] = ((T)(2 * (i % N)) / N) - 1;
-// 			}
-// 		}
-// 	}
-// }
+		while temp_clock_value == 1 {
+			temp_clock_value = *clock.lock().unwrap();
+		}		
+	}
+}
+
+pub fn generate_triangle_wave_data<T: Num + std::ops::Neg<Output = T> + std::fmt::Display + Copy + 'static>(vec: &mut Vec<T>
+		, buffer_len: usize
+		, clock: Arc<Mutex<i8>>
+		, period: i64)
+		
+		where i64: AsPrimitive<T> {
+
+	// get the current clock value
+	let mut val: T;
+	let value_guard = clock.lock().unwrap();
+	let mut temp_clock_value: i8 = *value_guard;
+	drop(value_guard);
+
+	for i in 0..buffer_len {
+		while temp_clock_value == 0 {
+			temp_clock_value = *clock.lock().unwrap();
+		}
+		if (i as i64 % period) < (period/2){
+			val = ((4*(i as i64 % period))/period - 1).as_();
+		} else {
+			val = (3 - (4*(i as i64 % period))/period).as_();
+		}
+		(*vec).push(val);
+
+		while temp_clock_value == 1 {
+			temp_clock_value = *clock.lock().unwrap();
+		}		
+	}
+}
+
+pub fn generate_sawtooth_wave_data<T: Num + std::ops::Neg<Output = T> + std::fmt::Display + Copy + 'static>(vec: &mut Vec<T>
+		, buffer_len: usize
+		, clock: Arc<Mutex<i8>>
+		, period: i64)
+		
+		where i64: AsPrimitive<T> {
+
+	// get the current clock value
+	let mut val: T;
+	let value_guard = clock.lock().unwrap();
+	let mut temp_clock_value: i8 = *value_guard;
+	drop(value_guard);
+
+	for i in 0..buffer_len {
+		while temp_clock_value == 0 {
+			temp_clock_value = *clock.lock().unwrap();
+		}
+		
+		val = (((2 * (i as i64 % period)) / period) - 1).as_();
+		(*vec).push(val);
+
+		while temp_clock_value == 1 {
+			temp_clock_value = *clock.lock().unwrap();
+		}		
+	}
+}
 
